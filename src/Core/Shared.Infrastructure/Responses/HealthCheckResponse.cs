d2096ddd4bfa6314c938d80f0ce5d834d641dbde
Nelson Nunes <nelson.nunes@eoh.com>
2025-02-26 13:01:17 +0200

    Refactored the /Shared folders into Core domain.

--- src/Core/Shared.Infrastructure/Responses/HealthCheckResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shared.Domain.Models;
-
-namespace Shared.Infrastructure.Responses;
-
-public class HealthCheckResponse
-{
-    public string Status { get; set; } = default!;
-    public required Dictionary<string, object> Checks { get; set; }
-}
--- src/Core/Shared/Abstractions/IValidateRocketOpsOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Shared.Abstractions;
-
-public interface IValidateRocketOpsOptions : IValidatableObject
-{
-    static abstract string ConfigurationKey { get; }
-}
